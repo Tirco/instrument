1834732c2c2a5e53978457acce3f1f16f2e0ac8e
Tirco <morten.liberg@gmail.com>
2022-04-21 09:29:47 +0200

    Småfiks

--- main.ts
+++ main.ts
@@ -8,20 +8,20 @@ input.onButtonPressed(Button.AB, function () {
     if (Spill == 0) {
         Spill = 1
         basic.showLeds(`
-            # # . . .
-            # # # # .
-            # # # # #
-            # # # # .
-            # # . . .
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
             `)
     } else {
         Spill = 0
         basic.showLeds(`
-            . . . . .
-            . # . # .
-            . # . # .
-            . # . # .
-            . . . . .
+            . # . . .
+            . # # . .
+            . # # # .
+            . # # . .
+            . # . . .
             `)
     }
 })
@@ -31,7 +31,7 @@ input.onButtonPressed(Button.B, function () {
 let Spill = 0
 let Oktav = 0
 Oktav = 4
-Spill = 0
+Spill = 1
 basic.forever(function () {
     if (Spill == 0) {
         music.setTempo(90 + input.rotation(Rotation.Pitch) * 2)
