fcf2e751200bd6a571b5989ecd1fcb7231bc59d9
Tirco <morten.liberg@gmail.com>
2022-04-21 09:10:37 +0200

    Startfiler for MakeCode-prosjektet

---
+++ main.ts
@@ -0,0 +1,40 @@
+input.onButtonPressed(Button.A, function () {
+    Oktav += -1
+})
+input.onGesture(Gesture.ScreenDown, function () {
+    Oktav = 4
+})
+input.onButtonPressed(Button.AB, function () {
+    if (Spill == 0) {
+        Spill = 1
+        basic.showLeds(`
+            # # . . .
+            # # # # .
+            # # # # #
+            # # # # .
+            # # . . .
+            `)
+    } else {
+        Spill = 0
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    Oktav += 1
+})
+let Spill = 0
+let Oktav = 0
+Oktav = 4
+Spill = 0
+basic.forever(function () {
+    if (Spill == 0) {
+        music.setTempo(90 + input.rotation(Rotation.Pitch) * 2)
+        music.playTone(45 * Oktav + input.rotation(Rotation.Roll) * 2, music.beat(BeatFraction.Half))
+    }
+})
